6161a6757ae10ac985cc60801b2478e27dc60b53
1103409364 <1103409364@qq.com>
2020-01-31 15:31:55 +0800

    Array

--- src/Array/index.js
+++ src/Array/index.js
@@ -29,12 +29,135 @@ export const letterCombinations = (digits) => {
     if (arr.length > 1) {
       comb(arr);
     }
-    // else {
-    //   return tmp;
-    // }
-
     return arr[0];
   };
 
   return comb(code);
+};
+
+/**
+ * @param {number[]} deck 一副牌
+ * @return {boolean}
+ */
+export const hasGroupsSizeX = (deck) => {
+  deck.sort((a, b) => a - b);
+  let min = Number.MAX_SAFE_INTEGER;
+  // 保存所有相同数字的分组，
+  let dst = [];
+  for (let i = 0, len = deck.length, tmp = []; i < len; i++) {
+    tmp.push(deck[i]);
+
+    for (let j = i + 1; j <= len; j++) {
+
+      if (deck[i] === deck[j]) {
+        tmp.push(deck[j]);
+
+      } else {
+        if (min > tmp.length) {
+          min = tmp.length;
+        }
+        dst.push(tmp.length); // 储存每个分组的长度
+        tmp.length = 0;
+        i = j - 1; // i应该应该从j开始，但本次循环结束 i++，应该-1，i+1 = j
+        break;
+      }
+    }
+  }
+
+  if (min < 2) return false;
+  // dst 的最大公约数
+  function gcd(a, b) {
+    if (a % b === 0) {
+      return b;
+    }
+    return gcd(b, a % b);
+  }
+
+  // forEach 无法跳出循环
+  dst.forEach(item => {
+    min = gcd(min, item);
+  });
+
+  return min > 1;
+};
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+export const canPlaceFlowers = (flowerbed, n) => {
+  // 考虑头尾连续0的情况，头尾补 0
+  flowerbed.push(0);
+  flowerbed.unshift(0);
+  let zeroArr = [];
+  for (let i = 0, len = flowerbed.length, tmp = []; i <= len; i++) {
+    if (flowerbed[i] === 0) {
+      tmp.push(0);
+    } else {
+      tmp.length >= 3 && zeroArr.push(tmp.length);
+      tmp.length = 0;
+    }
+  }
+  // 计算 n 个 0，能替换几个 1，头尾 0 不能替换
+  function count(arr) {
+    let sum = 0;
+    arr.forEach(item => {
+      sum += Math.round((item - 2) / 2);
+    });
+    return sum;
+  }
+
+  // console.log(count(zeroArr));
+  return n <= count(zeroArr);
+};
+// 解法 2
+export const canPlaceFlowers2 = (flowerbed, n) => {
+  // 考虑头尾连续0的情况，头尾补 0，规避边界问题
+  flowerbed.push(0);
+  flowerbed.unshift(0);
+  let max = 0;
+  for (let i = 1, len = flowerbed.length - 1; i < len; i++) {
+    if (flowerbed[i] === 0) {
+      if (flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
+        max++;
+        i++; //结合 for 循环中的 i++ 会跳过两个
+      }
+    }
+  }
+  // console.log(max);
+  return max >= n;
+};
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+export const grayCode = (n) => {
+  // 递归函数，用来算输入为n的格雷编码序列
+  let make = (n) => {
+    if (n === 0) {
+      return ['0'];
+    }
+    if (n === 1) {
+      return ['0', '1'];
+    } else {
+      let pre = make(n - 1);
+      let result = [];
+      // 最大 index
+      let max = Math.pow(2, n) - 1;
+      for (let i = 0, len = pre.length; i < len; i++) {
+        result[i] = `0${pre[i]}`;
+        result[max - i] = `1${pre[i]}`;
+      }
+      return result;
+    }
+  };
+  // 转为十进制
+  let gray = make(n).map(item => {
+    return parseInt(item, 2);
+  });
+  // console.log(gray);
+
+  return gray;
 };
--- test/Array/index.test.js
+++ test/Array/index.test.js
@@ -1,4 +1,4 @@
-import { letterCombinations } from '../../src/Array';
+import { letterCombinations, hasGroupsSizeX, canPlaceFlowers, grayCode } from '../../src/Array';
 
 test('letterCombinations: "23"', () => {
   expect(letterCombinations('23')).toEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']);
@@ -17,3 +17,18 @@ test('telComb:234', () => {
     'cfg', 'cfh', 'cfi'
   ]);
 });
+
+test('hasGroupsSizeX: [1,1,1,2,2,2,3,3]', () => {
+  expect(hasGroupsSizeX([1, 1, 1, 2, 2, 2, 3, 3])).toBe(false);
+});
+
+test('canPlaceFlowers([1,0,0,0,1], 1)', () => {
+  expect(canPlaceFlowers([1, 0, 0, 0, 1], 1)).toBe(true);
+});
+test('canPlaceFlowers([1,0,0,0,1], 2)', () => {
+  expect(canPlaceFlowers([1, 0, 0, 0, 1], 2)).toBe(false);
+});
+test('grayCode(2)', () => {
+  expect(grayCode(2)).toEqual([0, 1, 3, 2]);
+});
+
